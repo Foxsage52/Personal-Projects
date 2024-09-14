464fcd3f6f099b9927bb5d5fd57fdf6ec945cd94
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-14 01:21:14 -0400

    Update Testing.py

--- rpg_sim/Testing.py
+++ rpg_sim/Testing.py
@@ -1,4 +1,6 @@
 import Classes
 
-wizard = Classes.Wizard(level= 1, healthpoint = 10, attack = 10, defense = 5, mana = 10, dex = 20, speed = 15, experience = 0)
+wizard = Classes.Wizard(level= 1, experience = 0, healthpoint = 10, attack = 10, defense = 5, mana = 10, dex = 20, speed = 15)
+
+wizard.gain_experience(20)
 print(wizard)
