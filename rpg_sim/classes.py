a500e6853da593bd435695f6ed0f12555d5aac5b
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-14 17:02:52 -0400

    Update classes.py

--- rpg_sim/classes.py
+++ rpg_sim/classes.py
@@ -1,5 +1,6 @@
-import system
-class Warrior(system):
+from system import System
+
+class Warrior(System):
     def __init__(self, level = 1, experience = 0, healthpoint=100, attack=20, defense=15, m_defense=10, mana=5, speed=10):
         super().__init__(level, healthpoint, experience, attack, defense, m_defense, mana, speed)
     def level_up(self):
@@ -14,17 +15,17 @@ class Warrior(system):
 
     
 
-class Wizard(system):
+class Wizard(System):
     def __init__(self, level = 1, experience = 0, healthpoint=80, attack=15, defense=8, m_defense=12, mana=30, speed=12):
         super().__init__(level, healthpoint, experience, attack, defense, m_defense, mana, speed)
     
     
-class Rogue(system):
+class Rogue(System):
     def __init__(self, level = 1, experience = 0, healthpoint=90, attack=18, defense=12, m_defense=20, mana=10, speed=15):
         super().__init__(level, healthpoint, experience, attack, defense, m_defense, mana, speed)
      
 
-class Archer(system):
+class Archer(System):
     def __init__(self, level = 1, experience = 0, healthpoint=85, attack=17, defense=10, m_defense=18, mana=8, speed=14):
         super().__init__(level, healthpoint, experience, attack, defense, m_defense, mana, speed)
         
