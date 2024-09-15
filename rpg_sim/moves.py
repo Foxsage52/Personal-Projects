37d144712ab29788bc7f5ca1ea34748a21b7342c
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-14 20:54:08 -0400

    Update moves.py

--- rpg_sim/moves.py
+++ rpg_sim/moves.py
@@ -11,9 +11,9 @@ def update_warrior_moves(character):
     if isinstance(character, classes.Warrior):
         if character.level >= 5:
             character.moves["Shield Bash"] = {"type": "basic", "power": 15}
-            character.moves["War Cry"] = {"type": "special", "power": 0, "effect": "attack up"}
+            character.moves["War Cry"] = {"type": "special", "power": 10}
             print("Warrior learned Shield Bash and War Cry!")
         if character.level >= 10:
             character.moves["Cleave"] = {"type": "special", "power": 25}
-            character.moves["Rally"] = {"type": "buff", "power": 0, "effect": "defense up"}
-            print("Warrior learned Cleave and Rally!")
+            character.moves["Stab"] = {"type": "basic", "power": 15}
+            print("Warrior learned Cleave and Stab!")
