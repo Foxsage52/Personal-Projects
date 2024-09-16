c08171e604ece7a3d980e5298b27503a3ffbf32f
Foxsage52 <145737393+Foxsage52@users.noreply.github.com>
2024-09-16 15:35:53 -0400

    Update Testing.py
    Testing wizard xp

--- rpg_sim/Testing.py
+++ rpg_sim/Testing.py
@@ -5,9 +5,19 @@ warrior = classes.Warrior()
 
 # Print initial state
 print(warrior)
+print("")
 
 # Simulate gaining experience
 warrior.gain_experience(500)
 
 # Print updated state
 print(warrior)
+print("")
+
+wizard = classes.Wizard()
+
+print(wizard)
+print("")
+
+wizard.gain_experience(500)
+print(wizard)
